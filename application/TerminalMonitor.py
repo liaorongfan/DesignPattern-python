e06c6b71bd863eb8e371c2ca6f8aa120fab11270
liaorongfan <15670381505@163.com>
2021-09-15 00:37:34 +0800

    update

--- application/TerminalMonitor.py
+++ application/TerminalMonitor.py
@@ -6,6 +6,7 @@
 from pattern.Memento import Originator, Caretaker, Memento
 import logging
 
+
 class TerminalCmd(Originator):
     """终端命令"""
 
@@ -18,17 +19,17 @@ class TerminalCmd(Originator):
         """从字符串中解析命令"""
         subStrs = self.getArgumentsFromString(text, " ")
         # 获取第一个字段作为命令名称
-        if(len(subStrs) > 0):
+        if len(subStrs) > 0:
             self.__cmdName = subStrs[0]
 
         # 获取第一个字段之后的所有字符作为命令的参数
-        if (len(subStrs) > 1):
+        if len(subStrs) > 1:
             self.__cmdArgs = subStrs[1:]
 
     def getArgumentsFromString(self, str, splitFlag):
         """通过splitFlag进行分割，获得参数数组"""
 
-        if (splitFlag == ""):
+        if splitFlag == "":
             logging.warning("splitFlag 为空!")
             return ""
 
@@ -36,13 +37,14 @@ class TerminalCmd(Originator):
         result = []
         for item in data:
             item.strip()
-            if (item != ""):
+            if item != "":
                 result.append(item)
 
         return result;
 
     def showCmd(self):
         print(self.__cmdName, self.__cmdArgs)
+
 
 class TerminalCaretaker(Caretaker):
     """终端命令的备忘录管理类"""
@@ -52,26 +54,26 @@ class TerminalCaretaker(Caretaker):
         for key, obj in self._mementos.items():
             name = ""
             value = []
-            if(obj._TerminalCmd__cmdName):
+            if obj._TerminalCmd__cmdName:
                 name = obj._TerminalCmd__cmdName
-            if(obj._TerminalCmd__cmdArgs):
+            if obj._TerminalCmd__cmdArgs:
                 value = obj._TerminalCmd__cmdArgs
-            print("第%s条命令: %s %s" % (key, name, value) )
+            print("第%s条命令: %s %s" % (key, name, value))
 
 
 def testTerminal():
     cmdIdx = 0
     caretaker = TerminalCaretaker()
     curCmd = TerminalCmd("")
-    while (True):
+    while True:
         strCmd = input("请输入指令：");
         strCmd = strCmd.lower()
-        if (strCmd.startswith("q")):
+        if strCmd.startswith("q"):
             exit(0)
-        elif(strCmd.startswith("h")):
+        elif strCmd.startswith("h"):
             caretaker.showHistoryCmds()
         # 通过"!"符号表示获取历史的某个指令
-        elif(strCmd.startswith("!")):
+        elif strCmd.startswith("!"):
             idx = int(strCmd[1:])
             curCmd.restoreFromMemento(caretaker.getMemento(idx))
             curCmd.showCmd()
@@ -79,7 +81,7 @@ def testTerminal():
             curCmd = TerminalCmd(strCmd)
             curCmd.showCmd()
             caretaker.addMemento(cmdIdx, curCmd.createMemento())
-            cmdIdx +=1
+            cmdIdx += 1
 
 
 testTerminal()
